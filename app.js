6b7dd07e92bfd23ef6df144cbcc1ab01132aceba
Associado <leticiadiasp@gmail.com>
2021-03-31 12:23:41 -0300

    Projeto

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ app.post("/artigo", (req, res) => {
 });
 var express = require('express');
 var app = express();
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
+        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
+    else //Se a requisição já é HTTPS 
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+});
 
 //... your code here ...
                                 
